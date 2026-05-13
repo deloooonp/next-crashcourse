0fa5833f1d1153681b5dd1fcd96b5124dd9aa4ac
Delon Pratama <delonpratama1009@gmail.com>
2026-05-13 14:24:20 +0700

    feat: integrated better auth with convex with sign up feature

--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link";
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./theme-toggle";
+import { useConvexAuth } from "convex/react";
+import { authClient } from "@/lib/auth-client";
 
 export function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <nav className="w-full py-5 flex items-center justify-between">
       <div className="flex items-center gap-8">
@@ -24,15 +29,22 @@ export function Navbar() {
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <Link className={buttonVariants()} href="/auth/sign-up">
-          Sign Up
-        </Link>
-        <Link
-          className={buttonVariants({ variant: "secondary" })}
-          href="/auth/login"
-        >
-          Login
-        </Link>
+        {isLoading ? null : isAuthenticated ? (
+          <Button onClick={() => authClient.signOut({})}>Log out</Button>
+        ) : (
+          <>
+            <Link className={buttonVariants()} href="/auth/sign-up">
+              Sign Up
+            </Link>
+            <Link
+              className={buttonVariants({ variant: "secondary" })}
+              href="/auth/login"
+            >
+              Login
+            </Link>
+          </>
+        )}
+
         <ThemeToggle />
       </div>
     </nav>
