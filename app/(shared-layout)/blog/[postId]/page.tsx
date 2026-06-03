fb1410516effaa8c40bdbe8ea4cd670e62747dee
Delon Pratama <delonpratama1009@gmail.com>
2026-06-03 14:18:47 +0700

    feat: implemented single post page with server side fetching

---
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -0,0 +1,65 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { fetchQuery } from "convex/nextjs";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface PostIdRouteProps {
+  params: Promise<{
+    postId: Id<"posts">;
+  }>;
+}
+
+export default async function PostIdRoute({ params }: PostIdRouteProps) {
+  const { postId } = await params;
+
+  const post = await fetchQuery(api.posts.getPostById, { postId: postId });
+  if (!post) {
+    return (
+      <h1 className="text-6xl font-extrabold text-red-500 py-20">
+        No Post Found
+      </h1>
+    );
+  }
+  return (
+    <div className="max-w-3xl mx-auto py-8 px-4 animate-in fade-in duration-500 relative">
+      <Link
+        className={buttonVariants({
+          variant: "outline",
+          className: "mb-4",
+        })}
+        href={"/blog"}
+      >
+        <ArrowLeft className="size-4" /> Back to blog
+      </Link>
+
+      <div className="relative w-full h-100 mb-8 rounded-xl overflow-hidden shadow-sm">
+        <Image
+          src={
+            post.imageUrl ??
+            "https://images.unsplash.com/photo-1777691973962-12dfdd860807?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          fill
+          alt={post.title}
+          className="object-cover hover:scale-105 transition-transform duration-500"
+        />
+      </div>
+      <div className="space-y-4 flex flex-col">
+        <h1 className="text-4xl font-bold tracking-tight text-foreground">
+          {post.title}
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          Posted on : {new Date(post._creationTime).toLocaleDateString()}
+        </p>
+      </div>
+      <Separator className="h-px my-8" />
+      <p className="text-lg leading-relaxed text-foreground/90 whitespace-pre-wrap">
+        {post.body}
+      </p>
+      <Separator className="h-px my-8" />
+    </div>
+  );
+}
